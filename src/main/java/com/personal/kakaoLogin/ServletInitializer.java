481ab1f8968036d048c852d40e32cc0f00041ec6
= <=>
2019-03-22 20:00:33 +0900

    [Service Class]

---
+++ src/main/java/com/personal/kakaoLogin/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.personal.kakaoLogin;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KakaoLoginApplication.class);
+	}
+
+}
---
+++ src/main/java/com/personal/kakaoLogin/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.personal.kakaoLogin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/")
+	public String index() {
+		
+		return "index";
+	}
+	
+	@RequestMapping(value="/login")
+	public String login() {
+		
+		return "index";
+	}
+}
