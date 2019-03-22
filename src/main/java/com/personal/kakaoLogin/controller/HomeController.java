4a219db6aef80566457012fec09e21b4f97dcecc
= <=>
2019-03-22 22:27:46 +0900

    [Gson Add & KakaoAPI Service & HomeController Modify]

--- src/main/java/com/personal/kakaoLogin/controller/HomeController.java
+++ src/main/java/com/personal/kakaoLogin/controller/HomeController.java
@@ -1,11 +1,19 @@
 package com.personal.kakaoLogin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.personal.kakaoLogin.service.KakaoAPI;
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	private KakaoAPI kakao;
+	
 	@RequestMapping(value="/")
 	public String index() {
 		
@@ -13,7 +21,9 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value="/login")
-	public String login() {
+	public String login(@RequestParam("code") String code) {
+		String access_Token = kakao.getAccessToken(code);
+		System.out.println("controller access_token : " + access_Token);
 		
 		return "index";
 	}
