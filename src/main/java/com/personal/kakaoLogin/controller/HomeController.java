66b07b45ff2447a30da041ed6bb30471eecd7177
= <=>
2019-03-23 21:30:21 +0900

    [Kakao UserInfo & Logout]

--- src/main/java/com/personal/kakaoLogin/controller/HomeController.java
+++ src/main/java/com/personal/kakaoLogin/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.personal.kakaoLogin.controller;
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.personal.kakaoLogin.service.KakaoAPI;
 
@@ -21,10 +24,24 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value="/login")
-	public String login(@RequestParam("code") String code) {
+	public String login(@RequestParam("code") String code, HttpSession session) {
 		String access_Token = kakao.getAccessToken(code);
-		System.out.println("controller access_token : " + access_Token);
+		HashMap<String, Object> userInfo = kakao.getUserInfo(access_Token);
+		System.out.println("login Controller : " + userInfo);
 		
+		//	클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
+		if (userInfo.get("email") != null) {
+			session.setAttribute("userId", userInfo.get("email"));
+			session.setAttribute("access_Token", access_Token);
+		}
+		return "index";
+	}
+	
+	@RequestMapping(value="/logout")
+	public String logout(HttpSession session) {
+		kakao.kakaoLogout((String)session.getAttribute("access_Token"));
+		session.removeAttribute("access_Token");
+		session.removeAttribute("userId");
 		return "index";
 	}
 }
